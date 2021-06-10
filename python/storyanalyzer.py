e12c4c02d7d27197d53c7cb5b2588f397483366c
Spectral Artist <spectralartist@Elis-MacBook-Pro.local>
2021-06-10 17:57:12 -0500

    This commit adds csv outputs for Gen Stats and Tag Counts

--- python/storyanalyzer.py
+++ python/storyanalyzer.py
@@ -59,6 +59,33 @@ class StoryAnalyzer(webscraper.Webscraper):
     def get_gen_stats_file_path(self):
         return os.path.join(self.folder_path, self.name + " General Statistics.txt")
 
+    def write_gen_stats_csv(self):
+        with open(self.get_gen_stats_csv_file_path(), 'w') as csv_outfile:
+            csv_writer = csv.writer(csv_outfile, delimiter=',')
+
+            csv_writer.writerows([
+                    # Totals Sections
+                    ["Total Stories", str(self.sum_stories)],
+                    ["Total Chapters", str(self.sum_chapters)],
+                    ["Total Words", str(self.sum_words)],
+                    ["Total Views", str(self.sum_views)],
+                    ["Total Likes" + str(self.sum_likes)],
+                    ["Total Comments", str(self.sum_comments)],
+
+                    # Per Story Averages Section
+                    ["Avg. Chapters per Story", str(round(self.sum_chapters / self.sum_stories, 2))],
+                    ["Avg. Words per Story", str(int(self.sum_words / self.sum_stories))],
+                    ["Avg. Likes per Story", str(int(self.sum_likes / self.sum_stories))],
+                    ["Avg. Comments per Story", str(int(self.sum_comments / self.sum_stories))],
+
+                    # Per Chapter Averages Section
+                    ["Avg. Words per Chapter", str(int(self.sum_words / self.sum_chapters))]
+
+            ])
+    
+    def get_gen_stats_csv_file_path(self):
+        return os.path.join(self.folder_path, self.name + " General Statistics.csv")
+
     def write_tag_counts_readable(self):
         with open(self.get_tag_counts_file_path(), 'w') as outfile:
 
@@ -73,6 +100,29 @@ class StoryAnalyzer(webscraper.Webscraper):
 
     def get_tag_counts_file_path(self):
         return os.path.join(self.folder_path, self.name + " Tag Counts.txt")
+
+    def write_tag_counts_csv(self, ranked=False):
+        with open(self.get_tag_counts_csv_file_path(), 'w') as csv_outfile:
+
+            csv_writer = csv.writer(csv_outfile, delimiter=',')
+
+            if ranked:
+                csv_writer.writerow(["Rank", "Tag", "Times Used"])
+            else:
+                csv_writer.writerow(["Tag", "Times Used"])
+
+            index = 1
+
+            for tag, total in self.tags_counter.most_common():
+                if ranked:
+                    csv_writer.writerow([str(index), tag, str(total)])
+                    index += 1
+                else:
+                    csv_writer.writerow([tag, str(total)])
+
+
+    def get_tag_counts_csv_file_path(self):
+        return os.path.join(self.folder_path, self.name + " Tag Counts.csv")
 
     def count_by_chapter(self):
         data = json.load(open(self.get_datafile_path(), 'r'))
@@ -144,7 +194,9 @@ class StoryAnalyzer(webscraper.Webscraper):
 
     def run_all(self):
         self.write_gen_stats_readable()
+        self.write_gen_stats_csv()
         self.write_tag_counts_readable()
+        self.write_tag_counts_csv()
         self.count_by_chapter()
         self.write_by_chapter_readable()
         self.write_by_chapter_csv()
