d8512147ee357656b9c64aa6050a79341ddc7487
Eli Card <spectralartists@gmail.com>
2021-12-09 17:37:53 -0600

    This commit removes the last installation file for python

--- install.sh
+++
@@ -1,7 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/python" >/dev/null 2>&1 && pwd )"
-cd "$DIR"
-python3 -m venv ao3-env
-source "${DIR}/ao3-env/bin/activate"
-python3 -m pip install --upgrade pip
-python3 -m pip install -r ../requirements.txt
-deactivate
